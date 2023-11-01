b36509eb1186054a322805016cdbc035486c810a
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2023-11-01 13:17:43 +0000

    Added sum_of_numbers.py

---
+++ sum_of_numbers.py
@@ -0,0 +1,26 @@
+# Created by: Finn Kitor
+# Created on: November 1st, 2023
+# This program asks the user to enter a positive number
+# and then uses a loop to calculate and display the sum 
+# of all numbers from 0 until that number.
+
+def main():
+  # terminal will initialize the loop counter and sum
+  loop_counter = 0
+  sum = 0
+
+  # get the user number as a string
+  user_number = int(input("Enter a positive number: "))
+  print("")
+
+  # calculate the sum of all numbers from 0 to user number
+  while (loop_counter <= user_number):
+    sum = sum + loop_counter
+    print("Tracking {0} times through loop.".format(loop_counter))
+    loop_counter = loop_counter + 1
+  
+  print("")
+  print ("The sum of the numbers from 0 to {} is: {}.".format(user_number, sum))
+
+if __name__ == "__main__":
+  main()
